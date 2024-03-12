38b205f61ecbd05027edc26505f6010cad5421b3
Petros <markaryan.petr@mail.ru>
2024-03-12 22:38:06 +0400

    Добавил простой секундомер при помощи chronometr, Добавил отображение таймера из гитхаба

--- app/src/main/java/com/example/smartalarm/BlankFragmentStopwatch.kt
+++ app/src/main/java/com/example/smartalarm/BlankFragmentStopwatch.kt
@@ -1,14 +1,19 @@
 package com.example.smartalarm
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Chronometer
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
 class BlankFragmentStopwatch : Fragment() {
-
+    private lateinit var stopwatch: Chronometer
+    private var isRunning = false // Включён ли секундомер
+    private var offset: Long = 0 // базовое смещение
     companion object {
         fun newInstance() = BlankFragmentStopwatch()
     }
@@ -21,11 +26,45 @@ class BlankFragmentStopwatch : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_blank_stopwatch, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        stopwatch = view.findViewById(R.id.chronometer)
 
+        val startButton: Button = view.findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+            if (!isRunning) {
+                setBaseTime()
+                stopwatch.start()
+                isRunning = true
+            }
+        }
+
+        val pauseButton: Button = view.findViewById(R.id.pause_button)
+        pauseButton.setOnClickListener {
+            if (isRunning) {
+                saveOffset()
+                stopwatch.stop()
+                isRunning = false
+            }
+        }
+
+        val resetButton: Button = view.findViewById(R.id.reset_button)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+    private fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+        stopwatch.format = "%s:%s.%s"
+    }
+
+    private fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(BlankFragmentStopwatchViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
